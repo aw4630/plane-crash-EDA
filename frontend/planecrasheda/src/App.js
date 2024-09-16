7b277968b110530b82adc8d079cb72cc195405cc
Alan <aw4630@nyu.edu>
2024-09-16 18:18:02 -0400

    frontend v4

--- frontend/planecrasheda/src/App.js
+++ frontend/planecrasheda/src/App.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DatabasePage from './pages/DatabasePage';
 import DataAnalysisPage from './pages/DataAnalysisPage';
 
 function App() {
   return (
-    
-    <Router>
+    <Router basename="/">
       <Routes>
-        {/* Root Route: HomePage */}
         <Route path="/" element={<HomePage />} />
-        
-        {/* Database Route */}
         <Route path="/database" element={<DatabasePage />} />
-        
-        {/* Data Analysis Route */}
         <Route path="/analysis" element={<DataAnalysisPage />} />
-        
-        {/* Add more routes here as needed */}
       </Routes>
     </Router>
-
-    
   );
 }
 
