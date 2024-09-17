b07761691112e9f2d00d6234b9a6da402b0d5433
Alan <aw4630@nyu.edu>
2024-09-17 02:34:56 -0400

    Updated AWS RDS connection

--- frontend/planecrasheda/db/index.js
+++ frontend/planecrasheda/db/index.js
@@ -3,11 +3,11 @@ require('dotenv').config();  // Load environment variables
 
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'planecrasheda.claaskamsdc4.us-east-2.rds.amazonaws.com',
-  database: 'postgres',
-  password: 'Planecrasheda123!',
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 module.exports = pool;
