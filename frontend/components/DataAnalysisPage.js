2fc7cb8fdbf20e14aad78595b07511de03c04036
Alan <aw4630@nyu.edu>
2024-09-22 16:09:06 -0400

    added Power BI dashboard

--- frontend/components/DataAnalysisPage.js
+++ frontend/components/DataAnalysisPage.js
@@ -5,10 +5,21 @@ function DataAnalysisPage() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Data Analysis Page</h1>
-      {/*  Data Analysis Content Here */}
+      
+      {/* Embed Power BI Dashboard */}
+      <iframe
+        title="planecrasheda"
+        width="1200"
+        height="600"
+        src="https://app.powerbi.com/reportEmbed?reportId=a2c6767a-d2e2-48a5-bdfe-b3d374224b84&autoAuth=true&embeddedDemo=true"
+        frameBorder="0"
+        allowFullScreen="true"
+      ></iframe>
+
+      {/* Back to Home Link */}
       <div style={{ marginTop: '20px' }}>
         <Link href="/" style={{ fontSize: '18px', textDecoration: 'none' }}>
-            Back to Home
+          Back to Home
         </Link>
       </div>
     </div>
--- frontend/pages/analysis.js
+++ frontend/pages/analysis.js
@@ -1,5 +1,7 @@
 import DataAnalysisPage from '../components/DataAnalysisPage';
+import PowerBIEmbed from '../components/PowerBIEmbed';
 
 export default function Analysis() {
+
   return <DataAnalysisPage />;
 }
